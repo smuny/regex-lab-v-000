de978dc388b4cc8cb18ddbed81001aeba8a9dc4f
Sharon Mun <sharonymun@gmail.com>
2018-06-02 21:31:40 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+  word.match(/^[AEIOUaeiou]\w+/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.scan(/\b[Uu]n[a-z]*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.scan(/^[A-Z].+[.?!]$/) != [] ? true : false
 end
 
 def valid_phone_number?(phone)
-
+  phone.scan(/^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/) != [] ? true : false
 end
