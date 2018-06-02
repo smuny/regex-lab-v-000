4a41c7207ba59004104e935b8993a2e20854090f
Sharon Mun <sharonymun@gmail.com>
2018-06-02 21:06:40 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,24 +1,21 @@
 def starts_with_a_vowel?(word)
-word.match(/^[AEIOUaeiou]\w+/) ? true : false
+  word.match(/^[AEIOUaeiou]\w+/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-text.scan(/^[unUN]\w/)
+  text.scan(/^[unUN]\w/)
 #text.scan(/\b[Uu]n[a-z]*ing\b/)
 end
 
 def words_five_letters_long(text)
-text.match(/^{5}\w/)
+  text.match(/^{5}\w/)
 #text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-if text.scan(/^[A-Z].*[a-zA-Z][.]$/)
-  return true
-else false
-end
+  text.scan(/^[A-Z].*[a-zA-Z][.]$/)
 end
 
 def valid_phone_number?(phone)
-phone.scan(/^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/) ? true : false
+  phone.scan(/^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/) ? true : false
 end
