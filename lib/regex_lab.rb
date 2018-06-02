473bfece88b13816d163b7223510c4314d414204
Sharon Mun <sharonymun@gmail.com>
2018-06-02 21:05:34 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,8 +15,7 @@ end
 def first_word_capitalized_and_ends_with_punctuation?(text)
 if text.scan(/^[A-Z].*[a-zA-Z][.]$/)
   return true
-else
-  false
+else false
 end
 
 def valid_phone_number?(phone)
